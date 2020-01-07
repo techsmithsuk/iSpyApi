45688f4ca94414e83bcfba12aecf4000884d38c0
Alexandru Catarau <alexctrx@gmail.com>
2020-01-07 13:57:54 +0000

    #3 Hello World api

---
+++ src/main/java/com/techswitch/ispy/ISpyApplication.java
@@ -0,0 +1,14 @@
+package com.techswitch.ispy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISpyApplication.class, args);
+		System.out.println("Hello World!!!");
+	}
+
+}
---
+++ src/test/java/com/techswitch/ispy/ISpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techswitch.ispy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
