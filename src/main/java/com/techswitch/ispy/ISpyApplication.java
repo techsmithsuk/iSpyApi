42cab1cd09fb644a08b4b4516059adcdfc598824
Alexandru Catarau <alexctrx@gmail.com>
2020-01-08 16:44:19 +0000

    Codecov Setup Using Jacoco

--- src/main/java/com/techswitch/ispy/ISpyApplication.java
+++ src/main/java/com/techswitch/ispy/ISpyApplication.java
@@ -8,7 +8,7 @@ public class ISpyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ISpyApplication.class, args);
-		
+
 		System.out.println("Hello World!!!");
 	}
 }
