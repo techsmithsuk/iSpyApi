eeb35fa2f07256f33281bd50416a81597adfa39e
Kapil Krishna <kapil.krishna.yo@gmail.com>
2020-01-09 13:57:07 +0000

    Endpoint With JSON String

--- src/main/java/com/techswitch/ispy/controllers/HomeController.java
+++ src/main/java/com/techswitch/ispy/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.techswitch.ispy.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techswitch.ispy.models.Suspect;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @RequestMapping("/")
     public String home() {
-        return "Hello world!";
+        return "";
     }
 
-    @RequestMapping("/test")
-    public String test() {
-        return new Suspect().getName();
+    @RequestMapping("/suspect")
+    public String suspect() throws JsonProcessingException {
+        Suspect suspect = new Suspect();
+        return objectMapper.writeValueAsString(suspect);
     }
 }
