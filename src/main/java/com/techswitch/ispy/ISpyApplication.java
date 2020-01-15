1347caca195022e7df6964087dfbd3a78c1e366c
Alexandru Catarau <alexctrx@gmail.com>
2020-01-15 13:37:20 +0000

    Added default Active Profile and Test Profile

--- src/main/java/com/techswitch/ispy/ISpyApplication.java
+++ src/main/java/com/techswitch/ispy/ISpyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ISpyApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ISpyApplication.class, args);
 		
--- src/test/java/com/techswitch/ispy/config/IntegrationTestConfig.java
+++ src/test/java/com/techswitch/ispy/config/IntegrationTestConfig.java
@@ -3,18 +3,18 @@ package com.techswitch.ispy.config;
 import com.opentable.db.postgres.embedded.EmbeddedPostgres;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+
 import javax.sql.DataSource;
 import java.io.IOException;
 
-@Profile("IntegrationTestConfig")
+
 @TestConfiguration
-public class IntegrationTestConfig {
+public class IntegrationTestConfig{
 
     @Bean
-    @Primary
-    public DataSource getTestDataSource() throws IOException {
+    @Profile("testDataSource")
+    public DataSource getDataSource() throws IOException {
         EmbeddedPostgres pg = EmbeddedPostgres.start();
         return pg.getPostgresDatabase();
     }
