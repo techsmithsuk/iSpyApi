d32001a6e4d8d30f5c5b5314c57ce49092ee8d98
Alexandru Catarau <alexctrx@gmail.com>
2020-01-16 14:10:55 +0000

    Added integration test to handle internal server errror

--- src/test/java/com/techswitch/ispy/controllers/ReportControllerTest.java
+++ src/test/java/com/techswitch/ispy/controllers/ReportControllerTest.java
@@ -3,13 +3,18 @@ package com.techswitch.ispy.controllers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.techswitch.ispy.config.IntegrationTestConfig;
 import com.techswitch.ispy.models.Report;
+import com.techswitch.ispy.services.ReportService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -27,6 +32,9 @@ public class ReportControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ReportService reportService;
 
     @Test
     public void givenValidRequest_thenStatusIsCreated() throws Exception {
@@ -93,12 +101,25 @@ public class ReportControllerTest {
     }
 
     @Test
-    public void givenRequestWithInvalidJson_thenInternalServerError() throws Exception {
+    public void givenRequestWithInvalidJson_theStatusBadRequest() throws Exception {
         String requestJson ="{\"suspectId\":}";
         mockMvc.perform(post("http://localhost:8080/reports/create")
                 .contentType(APPLICATION_JSON)
                 .content(requestJson))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.Error").value("Bad Request"));
+    }
+
+    @Test
+    public void givenRequest_thenInternalServerError() throws Exception {
+        Report report = new Report(1L, "12-02-2019", "London", "description text");
+        String requestJson = objectMapper.writeValueAsString(report);
+
+        when(reportService.createReport(any(Report.class))).thenThrow(RuntimeException.class);
+        mockMvc.perform(post("http://localhost:8080/reports/create")
+                .contentType(APPLICATION_JSON)
+                .content(requestJson))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.Error").value("Internal Server Error"));
     }
 }
