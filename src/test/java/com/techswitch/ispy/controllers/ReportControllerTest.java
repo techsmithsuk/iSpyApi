115b6b16c9b6df37b431df47e0715436329a051a
Alexandru Catarau <alexctrx@gmail.com>
2020-01-16 14:33:29 +0000

    Created separate test for Internal Server Error

--- src/test/java/com/techswitch/ispy/controllers/ReportControllerTest.java
+++ src/test/java/com/techswitch/ispy/controllers/ReportControllerTest.java
@@ -8,13 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -32,9 +28,6 @@ public class ReportControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @MockBean
-    private ReportService reportService;
 
     @Test
     public void givenValidRequest_thenStatusIsCreated() throws Exception {
@@ -108,18 +101,5 @@ public class ReportControllerTest {
                 .content(requestJson))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.Error").value("Bad Request"));
-    }
-
-    @Test
-    public void givenRequest_thenInternalServerError() throws Exception {
-        Report report = new Report(1L, "12-02-2019", "London", "description text");
-        String requestJson = objectMapper.writeValueAsString(report);
-
-        when(reportService.createReport(any(Report.class))).thenThrow(RuntimeException.class);
-        mockMvc.perform(post("http://localhost:8080/reports/create")
-                .contentType(APPLICATION_JSON)
-                .content(requestJson))
-                .andExpect(status().isInternalServerError())
-                .andExpect(jsonPath("$.Error").value("Internal Server Error"));
     }
 }
